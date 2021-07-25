8d56ddabfa98e5447a971d78e12f9fe5a2a10e31
spblewis <spblewis@gmail.com>
2021-07-25 17:16:22 -0500

    Learned about images in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
-            }
-        ]
-    }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
